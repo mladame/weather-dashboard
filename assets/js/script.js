56f631b7371523998aafe79a529b066fa2bce66a
Martha A <mladame43@gmail.com>
2022-08-29 16:46:52 -0700

    expanded pseudocode; basic html body structure added with classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,9 @@
 //      call geolocator api, then call weather api
 //      onclick search button, fetch api, json(), display results
 //   display city weather api
-//      city, date, day/night; temp, wind, humidity, uv index(color coded); 5-day forecast
-//   list local storage
+//      city, date, day/night; icons; temp, wind, humidity, uv index(color coded); 5-day forecast
+//      uv index: green = favorable; yellow = moderate; red = severe
+//   list local storage - search history
 //      previously searched cities remain on list despite refresh, display upon page open
 //      weather info clears upon refresh
+//      can click on cities and get info, will overwrite what is being shown
