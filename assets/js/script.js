597625a5b422a87f167cd5b8faf287d53b80d059
Martha A <mladame43@gmail.com>
2022-09-07 16:54:27 -0700

    replaced javascript with jquery, linked jquery to index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 // Pseudocode
 // need:
 //   search for city
-//      call geolocator api, then call weather api
+//      call geolocator api
+//          
+//       then call weather api
 //      onclick search button, fetch api, json(), display results
 //      createEl
 //      geolocator api call: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
@@ -18,20 +20,31 @@
 
 // Start Code
 // DEFINE ELEMENTS
-const searchBtn = document.getElementById('#search-btn');
+const searchBtn = $("#search-btn");
+const cityInput = $(".city-input");
+
 
 // FETCH API
-searchBtn.addEventListener('click', fetchWeather);
-fetchWeather() {
-    var geocoderURL = "https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=d2cb5b734a2fa9d859a2d482475acef1"
+// searchBtn.addEventListener("click", fetchWeather);
+// function fetchWeather(query) {
+
+//     var geocoderURL = "https://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=d2cb5b734a2fa9d859a2d482475acef1"
+
+//         fetch(geocoderURL)
+//         .then(response => response.json())
+//         .then(data => console.log(data));
+
+//     var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=97b3a3279f4bc24383eff898e8ad790c"
 
 
-    var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=97b3a3279f4bc24383eff898e8ad790c"
+// }
 
-
-}
+searchBtn.on("click", function(event){
+    event.preventDefault();
+    console.log(event);
+})
 
 // RENDER WEATHER
-renderWeather() {
+// function renderWeather() {
 
-}
+// }
