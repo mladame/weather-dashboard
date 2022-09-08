decfe5f8fa97896c98fb06164b0b6c0ffc67e6a5
Martha A <mladame43@gmail.com>
2022-09-07 22:51:21 -0700

    pushed data into geocodeData arr successfully

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,35 +21,43 @@
 // Start Code
 // DEFINE ELEMENTS
 const searchBtn = $("#search-btn");
+    // let cityName = $("city-name");
 
+let geocodeData = [];
 
 
 // FETCH API
 searchBtn.on("click", function(query){
-    // event.preventDefault();
 
     // Define user input for desired city
     let cityInput = $("#city-input");
     // iterate over cityInput's keys, logs key names and respective values to console
     for(let key in cityInput) {
-        console.log(key + ":", cityInput[key].value);
+        // console.log(key + ":", cityInput[key].value);
     }
 
-    console.log(cityInput);
-
-    // let cityName = $("city-name");
-    // let geocodeData = [];
-
+    // api url to get lat + lon for desired city
     let geocoderURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityInput[0].value + '&appid=d2cb5b734a2fa9d859a2d482475acef1'
 
     // fetch api for geocoder, user inputs desired City, geocoder returns corresponding lat + lon
     fetch(geocoderURL)
         .then(response => response.json())
-        .then(data => console.log(data));
-        
+        .then(data => geocodeData.push(data[0]));
+
+        console.log(geocodeData);
+
+        // loop through geocodeData, define lat and lon, put into weather api call
         
 
-    let weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=97b3a3279f4bc24383eff898e8ad790c"
+        // define lat and lon
+        // let cityLat = $('geocodeData[0].lat');
+        // let cityLon = $('geocodeData[0].lon');
+
+        // console.log(cityLat);
+        // console.log(cityLon);
+
+
+    // let weatherURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' +  + '&lon=' +  + '&exclude=hourly,daily&appid=97b3a3279f4bc24383eff898e8ad790c'
 
     // fetch weather api, input lat + lon for desired City, return weather report
     // fetch(weatherURL)
