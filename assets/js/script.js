a13aa74f8311edc2cd015af2a16ca175138ecc1c
Martha A <mladame43@gmail.com>
2022-09-11 23:23:35 -0700

    current weather icon working

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ const cUVI = $("#c-uvi");
 const displayHistory = $("#city-name");
 
 const currentDate = moment().format("MM/DD/YYYY");
-const fDate = $(".f-date");
+// const fDate = $(".f-date");
 
 let cityList = [];
 
@@ -32,8 +32,8 @@ let cityList = [];
 searchBtn.on("click", function(query){
 
     // Define user input for desired city
-    let cityInput = $("#city-input").val().trim();
-    console.log(cityInput);
+        let cityInput = $("#city-input").val().trim();
+        console.log(cityInput);
 
     
     // Local Storage
@@ -42,61 +42,69 @@ searchBtn.on("click", function(query){
         console.log(localStorage);
 
     // api url to get lat + lon for desired city
-    let geocoderURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityInput + '&appid=d2cb5b734a2fa9d859a2d482475acef1'
+        let geocoderURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityInput + '&appid=d2cb5b734a2fa9d859a2d482475acef1'
 
     // fetch api for geocoder, user inputs desired City, geocoder returns corresponding lat + lon
-    fetch(geocoderURL)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data[0]);
+        fetch(geocoderURL)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data[0]);
 
             // Define lat and lon
-            let geoLat = data[0].lat;
-            let geoLon = data[0].lon;
+                let geoLat = data[0].lat;
+                let geoLon = data[0].lon;
 
             // Define + display city + current date
-            const cityName = data[0].name;
-            cityDate.text(cityName + "  " + currentDate);
+                const cityName = data[0].name;
+                cityDate.text(cityName + "  " + currentDate);
 
-            let startDate = moment();
-            let forecastArr = [];
-            // forecastDates = document.createElement("h3");
-            
-            for(let i=0; i<5; i++){
-            forecastDates = startDate.add( + 1, 'days').format("MM/DD/YYYY");
-            forecastArr.push(forecastDates);
-            console.log(forecastDates);
-            console.log(forecastArr);
-            // fDate.text(forecastDates);
-            $("#day1").text(forecastArr[0]);
-            $("#day2").text(forecastArr[1]);
-            $("#day3").text(forecastArr[2]);
-            $("#day4").text(forecastArr[3]);
-            $("#day5").text(forecastArr[4]);
-            }
+
 
             // fetch weather api using values pulled from geocoder api call
-            let weatherURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + geoLat + '&lon=' + geoLon + '&exclude=minutely,hourly,alerts&units=imperial&appid=9b35244b1b7b8578e6c231fd7654c186'
+                let weatherURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + geoLat + '&lon=' + geoLon + '&exclude=minutely,hourly,alerts&units=imperial&appid=9b35244b1b7b8578e6c231fd7654c186'
 
             // fetch weather api, input lat + lon for desired City, return weather report
-            fetch(weatherURL)
-                .then(response => response.json())
-            .then(data => {
+                fetch(weatherURL)
+                    .then(response => response.json())
+                .then(data => {
 
                 // console.log(data.current.weather[0].main);
-                let weatherIcon = data.current.weather[0].main;
-                let forecast = data.daily;
-                console.log(forecast);
-                cTemp.text("Temp: " + data.current.temp + " °F");
-                cWind.text("Wind Speed: " + data.current.wind_speed + " mph");
-                cHumidity.text("Humidity: " + data.current.humidity + "%");
-                cUVI.text("UV Index: " + data.current.uvi);
+                console.log(data.current);
+                    let weatherIcon = data.current.weather[0].icon;
+                    var iconURL = "http://openweathermap.org/img/wn/" + weatherIcon + ".png";                
+                    let forecastData = []
+                    let forecast = data.daily;
+                    forecastData.push(forecast);
+                    console.log(forecast);
+                
+                // set current weather conditions
+                    cTemp.text("Temp: " + data.current.temp + " °F");
+                    cWind.text("Wind Speed: " + data.current.wind_speed + " mph");
+                    cHumidity.text("Humidity: " + data.current.humidity + "%");
+                    cUVI.text("UV Index: " + data.current.uvi);
+                    $("#c-icon").html("<img src=" + iconURL + ">");
+                
+                // set forecast dates
+                    let startDate = moment();
+                    let forecastArr = [];
+                    
+                    for(let i=0; i<5; i++){
+                    forecastDates = startDate.add( + 1, 'days').format("MM/DD/YYYY");
+                    forecastArr.push(forecastDates);
+                    $("#day1").text(forecastArr[0]);
+                    $("#day2").text(forecastArr[1]);
+                    $("#day3").text(forecastArr[2]);
+                    $("#day4").text(forecastArr[3]);
+                    $("#day5").text(forecastArr[4]);
+                    }                
 
-                console.log(weatherIcon);
+
                 
                 // if(weatherIcon === "Clear"){
                 //     console.log("hi");
-                //     cIcon.classList.toggle("fa-sun");
+                //     // cIcon.child.addClass("fa-sun");
+                //     document.getElementById("c-icon").childNodes[0].className += " fa-sun";
+                //     cIcon
                 // } else if(weatherIcon === "Drizzle") {
                 //     cIcon.addClass("fa-cloud-drizzle");
                 // } else if(weatherIcon === "Rain") {
@@ -112,16 +120,16 @@ searchBtn.on("click", function(query){
                 //     cIcon.addClass("fa-smoke");
                 // };
 
-                if (data.current.uvi < "3" || data.current.uvi === "0") {
-                    cUVI.addClass("favorable");
-                } else if (data.current.uvi > "5") {
-                    cUVI.addClass("severe");
-                } else {
-                    cUVI.addClass("moderate");
-                }
+                    if (data.current.uvi < "3" || data.current.uvi === "0") {
+                        cUVI.addClass("favorable");
+                    } else if (data.current.uvi > "5") {
+                        cUVI.addClass("severe");
+                    } else {
+                        cUVI.addClass("moderate");
+                    }
 
                 // Populate 5-Day Forecast
-                for(var i=0; i < 5; i++) {
+                    for(var i=0; i < 5; i++) {
 
                 // Define Data for forecast
                     let fTemps = forecast[i].temp.day;
@@ -161,29 +169,13 @@ searchBtn.on("click", function(query){
                     //     pentIcon.addClass("fa-smoke");
                     // };
                 
-                }
+                    }
 
-            });
+                });
 
-        })
-        
+            })
 
-
-
-    // Define value and key, set to local storage
-    // var city = $(this).siblings("#city-input").val();
-    // var name = $(this).parent().attr("id");
-    // localStorage.setItem(name, JSON.stringify(city));
-
-    // cityList.push(JSON.parse(localStorage.getItem("city-name")));
-    // console.log(cityList);
-
-    // // create new element from local storage
-    // const searches = document.createElement("li").addClass("sh my-2");
-    // searches.textContent = localstorage.getItem("city-name");
-    // document.body.appendChild(searches);
-
-        // cityInput.val("");
+        cityInput.val("");
 })
 
 // RENDER LOCAL STORAGE
