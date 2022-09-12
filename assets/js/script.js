734cb41d6bc8b1463851098bbd0270b317bcc6c2
Martha A <mladame43@gmail.com>
2022-09-11 22:28:51 -0700

    Dates are now showing correct dates for 5-day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,11 +16,12 @@ const cTemp = $("#c-temp");
 const cWind = $("#c-wind");
 const cHumidity = $("#c-humidity");
 const cUVI = $("#c-uvi");
-const forecastCards = $(".forecast");
-const pentIcon = $(".f-icon");
-const pentTemp = $(".f-temp");
-const pentWind = $(".f-wind");
-const pentHumid = $(".f-humid");
+// const forecastCards = $(".forecast");
+// const pentIcon = $(".f-icon");
+// const pentTemp = $(".f-temp");
+// const pentWind = $(".f-wind");
+// const pentHumid = $(".f-humid");
+const displayHistory = $("#city-name");
 
 const currentDate = moment().format("MM/DD/YYYY");
 const fDate = $(".f-date");
@@ -31,51 +32,47 @@ let cityList = [];
 searchBtn.on("click", function(query){
 
     // Define user input for desired city
-    let cityInput = $("#city-input");
+    let cityInput = $("#city-input").val().trim();
+    console.log(cityInput);
+
     
     // Local Storage
-    
-    // let searchHistory = $("#city-name");
-
-
-    // let searchHistory = JSON.parse(localStorage.getItem("cityList"));
-
-    // console.log(searchHistory);
-
-        cityList.push(cityInput.val().trim());
+        cityList.push(cityInput);
         localStorage.setItem("cityList", JSON.stringify(cityList));
         console.log(localStorage);
 
-
-
-    // iterate over cityInput's keys, logs key names and respective values to console
-    for(let key in cityInput) {
-        // console.log(key + ":", cityInput[key].value);
-    }
-
     // api url to get lat + lon for desired city
-    let geocoderURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityInput[0].value + '&appid=d2cb5b734a2fa9d859a2d482475acef1'
+    let geocoderURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityInput + '&appid=d2cb5b734a2fa9d859a2d482475acef1'
 
     // fetch api for geocoder, user inputs desired City, geocoder returns corresponding lat + lon
     fetch(geocoderURL)
         .then((response) => response.json())
         .then((data) => {
             console.log(data[0]);
+
             // Define lat and lon
             let geoLat = data[0].lat;
             let geoLon = data[0].lon;
+
             // Define + display city + current date
             const cityName = data[0].name;
             cityDate.text(cityName + "  " + currentDate);
-            // var name = $("#city-name").attr("id");
-            // localStorage.setItem(name, JSON.stringify(cityName));
-            
-            // $("#city-name cityName").val(localStorage.getItem("city-name"));
+
+            let startDate = moment();
+            let forecastArr = [];
+            // forecastDates = document.createElement("h3");
             
             for(let i=0; i<5; i++){
-            let startDate = moment();                
-            let incDays = startDate.add(i + 1, 'days').format("MM/DD/YYYY");
-            fDate.text(incDays);                
+            forecastDates = startDate.add( + 1, 'days').format("MM/DD/YYYY");
+            forecastArr.push(forecastDates);
+            console.log(forecastDates);
+            console.log(forecastArr);
+            // fDate.text(forecastDates);
+            $("#day1").text(forecastArr[0]);
+            $("#day2").text(forecastArr[1]);
+            $("#day3").text(forecastArr[2]);
+            $("#day4").text(forecastArr[3]);
+            $("#day5").text(forecastArr[4]);
             }
 
             // fetch weather api using values pulled from geocoder api call
@@ -86,7 +83,8 @@ searchBtn.on("click", function(query){
                 .then(response => response.json())
             .then(data => {
 
-                let weatherIcon = JSON.stringify(data.current.weather[0].main);
+                // console.log(data.current.weather[0].main);
+                let weatherIcon = data.current.weather[0].main;
                 let forecast = data.daily;
                 console.log(forecast);
                 cTemp.text("Temp: " + data.current.temp + " °F");
@@ -95,29 +93,24 @@ searchBtn.on("click", function(query){
                 cUVI.text("UV Index: " + data.current.uvi);
 
                 console.log(weatherIcon);
-                console.log(forecast);
-                var fIcon = 
-                forecast[i].weather[0].main;
-                console.log(fIcon);
                 
-                
-                if((weatherIcon || fIcon) === "Clear"){
-                    console.log("hi");
-                    cIcon.addClass("fa-sun");
-                } else if(weatherIcon === "Drizzle") {
-                    cIcon.addClass("fa-cloud-drizzle");
-                } else if(weatherIcon === "Rain" || fIcon === "Rain") {
-                    console.log("rain, rain");
-                    cIcon.addClass("fa-cloud-showers-heavy");
-                } else if(weatherIcon === "Thunderstorm") {
-                    cIcon.addClass("fa-cloud-bolt");
-                } else if(weatherIcon === "Clouds") {
-                    cIcon.addClass("fa-cloud");
-                } else if(weatherIcon === "Snow") {
-                    cIcon.addClass("fa-cloud-snow");
-                } else if(weatherIcon === "Atmosphere") {
-                    cIcon.addClass("fa-smoke");
-                };
+                // if(weatherIcon === "Clear"){
+                //     console.log("hi");
+                //     cIcon.classList.toggle("fa-sun");
+                // } else if(weatherIcon === "Drizzle") {
+                //     cIcon.addClass("fa-cloud-drizzle");
+                // } else if(weatherIcon === "Rain") {
+                //     // console.log("rain, rain");
+                //     cIcon.addClass("fa-cloud-showers-heavy");
+                // } else if(weatherIcon === "Thunderstorm") {
+                //     cIcon.addClass("fa-cloud-bolt");
+                // } else if(weatherIcon === "Clouds") {
+                //     cIcon.addClass("fa-cloud");
+                // } else if(weatherIcon === "Snow") {
+                //     cIcon.addClass("fa-cloud-snow");
+                // } else if(weatherIcon === "Atmosphere") {
+                //     cIcon.addClass("fa-smoke");
+                // };
 
                 if (data.current.uvi < "3" || data.current.uvi === "0") {
                     cUVI.addClass("favorable");
@@ -130,10 +123,14 @@ searchBtn.on("click", function(query){
                 // Populate 5-Day Forecast
                 for(var i=0; i < 5; i++) {
 
-                    // Define Data for forecast
+                // Define Data for forecast
                     let fTemps = forecast[i].temp.day;
                     let fWind = forecast[i].wind_speed;
                     let fHumidity = forecast[i].humidity;
+
+                    console.log(fTemps);
+                    console.log(fWind);
+                    console.log(fHumidity);
 
                     // let startDate = moment();
                     // let incDays = startDate.add(i + 1, 'days').format("MM/DD/YYYY");
@@ -144,9 +141,9 @@ searchBtn.on("click", function(query){
                     // console.log(pentDates);
                     
                     // fDate.text(incDays);
-                    pentTemp.text("Temp: " + fTemps);
-                    pentWind.text("Wind: " + fWind + " mph");
-                    pentHumid.text("Humidity: " + fHumidity + "%");
+                    // pentTemp.text("Temp: " + fTemps);
+                    // pentWind.text("Wind: " + fWind + " mph");
+                    // pentHumid.text("Humidity: " + fHumidity + "%");
 
                     // if(fIcon === "clear"){
                     //     pentIcon.addClass("fa-sun");
@@ -182,12 +179,33 @@ searchBtn.on("click", function(query){
     // console.log(cityList);
 
     // // create new element from local storage
-    // const searches = document.createElement("p");
+    // const searches = document.createElement("li").addClass("sh my-2");
     // searches.textContent = localstorage.getItem("city-name");
     // document.body.appendChild(searches);
 
-        cityInput.val("");
+        // cityInput.val("");
 })
+
+// RENDER LOCAL STORAGE
+function renderLocalStorage() {
+
+    let searchHistory = JSON.parse(localStorage.getItem("cityList"));
+    console.log(searchHistory);
+
+    if(!cityList) {
+
+    }
+    
+    // searchHistory.forEach(displayHistory => {
+
+    // });
+    // for(let i=0; i < searchHistory.length; i++) {
+    //     const searches = document.createElement("li");
+    //     searches.textContent = searchHistory[0];
+    //     displayHistory.appendChild(searches).addClass("sh my-2");
+    // }
+
+}
 
 const clearBtn = $("#clear-btn");
 
